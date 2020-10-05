2a1cfaf48f2c4bba1d09881936af8025c4d51dff
ralamo33 <ralamo33@gmail.com>
2020-10-05 01:17:41 -0400

    Downloads card into directory, uplaods to s3, then removes local directory

--- DownloadCards.py
+++ DownloadCards.py
@@ -1,6 +1,7 @@
 import requests
 import boto3
 import shutil
+import os
 
 s3 = boto3.client('s3')
 bucket = 'yugiohcardimages'
@@ -18,19 +19,25 @@ if resp.status_code != 200:
 resp_dict = resp.json()
 card_data_list = resp_dict.get('data')
 image_urls = []
+dir = 'card_images'
+
+if not os.path.exists(dir):
+    os.makedirs(dir)
 
 for card_data in card_data_list:
     image_urls.append(card_data.get('card_images')[0].get("image_url"))
 
-image_url = image_urls[0]
-filename = image_url.split("/")[-1]
+
+image_url = image_urls[2]
+filename = dir + '/' + image_url.split("/")[-1]
 
 r = requests.get(image_url, stream = True)
 
 if r.status_code == 200:
     r.raw.decode_content = True
 
-    with open('card.jpg', 'wb') as f:
+    with open(filename, 'wb') as f:
         shutil.copyfileobj(r.raw, f)
+        s3.upload_file(filename, bucket, filename)
 
-s3.upload_file(filename, bucket, filename)
+shutil.rmtree(dir)
