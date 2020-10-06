8c255d815b9eb17c2afff6083bffa2800ff0b2ec
ralamo33 <ralamo33@gmail.com>
2020-10-06 00:34:29 -0400

    Downloads card into directory, uplaods to s3, then removes local directory

--- DownloadCards.py
+++ DownloadCards.py
@@ -27,17 +27,17 @@ if not os.path.exists(dir):
 for card_data in card_data_list:
     image_urls.append(card_data.get('card_images')[0].get("image_url"))
 
+for image_url in image_urls:
+    filename = dir + '/' + image_url.split("/")[-1]
 
-image_url = image_urls[2]
-filename = dir + '/' + image_url.split("/")[-1]
+    r = requests.get(image_url, stream = True)
 
-r = requests.get(image_url, stream = True)
+    if r.status_code == 200:
+        r.raw.decode_content = True
 
-if r.status_code == 200:
-    r.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            s3.upload_file(filename, bucket, filename)
 
-    with open(filename, 'wb') as f:
-        shutil.copyfileobj(r.raw, f)
-        s3.upload_file(filename, bucket, filename)
-
+print('yuGiOh cards uploaded to s3 bucket {}'.format(bucket))
 shutil.rmtree(dir)
